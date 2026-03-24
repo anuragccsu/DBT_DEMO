5ee501a6b4dc3f86a81da51abd304df15e0f8c65
Anurag Vashisth <anuragccsu@gmail.com>
2026-03-24 12:32:40 +0000

    committing changes.

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -11,7 +11,7 @@ with CTE As (
     {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 
     from {{ source('demo', 'bike') }}
-    where STARTED_AT != 'started at'
+    where STARTED_AT != 'started_at'
 )
 
 select * from CTE
